18cab1fac1eb6420e864c3cc2e37b2eb7f519f0c
Rashid Barket <37011882+rbarket@users.noreply.github.com>
2020-10-22 14:36:41 -0700

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import time
+from implementations import all_implementations
+
+df=pd.DataFrame()
+
+samples = 45
+timestart = time.time()
+for sort in all_implementations:
+    
+    tests = []
+    for trial in range(samples):
+        random_array=np.random.randint(100000, size=(20000))
+        st = time.time()
+        res = sort(random_array)
+        en = time.time()
+        timer = en-st
+        tests.append(timer)
+        col = pd.Series(tests).rename(str(sort))
+    df = pd.concat([df,col], axis=1)
+timeend = time.time()
+totaltime = timeend-timestart
+data.to_csv('data.csv', index=False)
