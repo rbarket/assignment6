9b39504fcec9ca7b9f38dab8eb45562f522c6969
Rashid Barket <37011882+rbarket@users.noreply.github.com>
2020-10-23 16:16:58 -0700

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -9,6 +9,7 @@ samples = 45
 timestart = time.time()
 names=['qs1','qs2','qs3','qs4','qs5','merge1','partition']
 for i in range(len(all_implementations)):
+    print(f'test {names[i]}')
     tests = []
     for trial in range(samples):
         random_array=np.random.randint(100000, size=(12250))
