ae7f4e661cde7253781891cca429acf10006d5ee
Rashid Barket <37011882+rbarket@users.noreply.github.com>
2020-10-23 17:20:45 -0700

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -5,14 +5,14 @@ from implementations import all_implementations
 
 df=pd.DataFrame()
 
-samples = 55
+samples = 50
 timestart = time.time()
 names=['qs1','qs2','qs3','qs4','qs5','merge1','partition']
 for i in range(len(all_implementations)):
     print(f'test {names[i]}')
     tests = []
     for trial in range(samples):
-        random_array=np.random.randint(100000, size=(12250))
+        random_array=np.random.randint(100000, size=(10000))
         st = time.time()
         res = all_implementations[i](random_array)
         en = time.time()
